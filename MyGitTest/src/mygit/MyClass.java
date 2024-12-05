33ca1bb58fa607871f327f9ee214dd6f82438f3e
scson <scson@DESKTOP-7PGQ8AQ>
2024-12-05 13:58:20 +0900

    first commit in dev branch

--- MyGitTest/src/mygit/MyClass.java
+++ MyGitTest/src/mygit/MyClass.java
@@ -4,5 +4,8 @@ public class MyClass {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
 		System.out.println("Hello World!");
+		System.out.println("This is my 2nd version");
+		YourClass your = new YourClass();
+		your.sayHello("everyone");
 		}
 }
